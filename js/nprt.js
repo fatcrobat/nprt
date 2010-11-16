304922f956a3493ac8f062018843b03e3c61f464
rico <rico@immix.fritz.box>
2010-11-17 00:21:26 +0100

    tablesorter pager customized

--- js/nprt.js
+++ js/nprt.js
@@ -6,4 +6,12 @@ Drupal.behaviors.nprtinit = function(context){
          filterClearContainer: $("#filter-clear-button"),
          filterColumns: [0,1,2,3],
          filterCaseSensitive: false});
+	 
+	 $("#attendance")
+	 .tablesorter({debug: false, widgets: ['zebra'], sortList: [[4,0],[0,0]]})
+	 .tablesorterPager({container: $("#pager"), positionFixed: false, size: 25})
+     .tablesorterFilter({filterContainer: $("#filter-box"),
+         filterClearContainer: $("#filter-clear-button"),
+         filterColumns: [0,1,2,3],
+         filterCaseSensitive: false});
 };
